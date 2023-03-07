f14afea42bf90d78711be673da1dbe1a8cd766bc
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2023-03-07 14:55:17 -0800

    Add variable declarations and event listeners. Set up structure and styles for options (need imgs)

--- scripts.js
+++ scripts.js
@@ -6,3 +6,16 @@ decide if user lose, win, or tie
 display choices and result
 reset btn clears all to start over
 */
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const options = document.querySelectorAll(".option");
+
+// APPLY EVENT LISTENER TO OPTIONS
+options.forEach(addEventListener("click", playerChoice()));
+
+// PLAYER CHOICE FUNC
+const playerChoice = () => {};
+
+// PC CHOICE FUNC
+// const compChoice = () => {};
