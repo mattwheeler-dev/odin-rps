a06d24ccb91a647bbc313b212d0513bdf0088c0d
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-02-26 14:19:45 -0800

    refactor HTML; remove styles for removed elements;

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
-const options = document.querySelectorAll(".option");
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-const result = document.querySelector("#result");
-const playerDisplay = document.querySelector("#player-display");
-const pcDisplay = document.querySelector("#pc-display");
+const options = document.querySelectorAll('.option');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const result = document.querySelector('#result');
+const playerDisplay = document.querySelector('#player-display');
+const pcDisplay = document.querySelector('#pc-display');
 let playerChoice;
 let pcChoice;
 
 // DECIDE WINNER AND DISPLAY RESULT
 const decideWinner = () => {
 	if (playerChoice.toLowerCase() == pcChoice.toLowerCase()) {
-		result.textContent = "Result: Tie!";
+		result.textContent = 'Result: Tie!';
 	} else if (
-		(playerChoice == "rock") & (pcChoice == "Scissors") ||
-		(playerChoice == "paper") & (pcChoice == "Rock") ||
-		(playerChoice == "scissors") & (pcChoice == "Paper")
+		(playerChoice == 'rock') & (pcChoice == 'Scissors') ||
+		(playerChoice == 'paper') & (pcChoice == 'Rock') ||
+		(playerChoice == 'scissors') & (pcChoice == 'Paper')
 	) {
-		result.textContent = "Result: YOU WIN!";
+		result.textContent = 'Result: YOU WIN!';
 	} else if (
-		(pcChoice == "Rock") & (playerChoice == "scissors") ||
-		(pcChoice == "Paper") & (playerChoice == "rock") ||
-		(pcChoice == "Scissors") & (playerChoice == "paper")
+		(pcChoice == 'Rock') & (playerChoice == 'scissors') ||
+		(pcChoice == 'Paper') & (playerChoice == 'rock') ||
+		(pcChoice == 'Scissors') & (playerChoice == 'paper')
 	) {
-		result.textContent = "Result: You lose...";
+		result.textContent = 'Result: You lose...';
 	}
 };
 
@@ -32,11 +32,11 @@ const computerDecision = () => {
 	const optionNumber = Math.floor(Math.random() * options.length);
 
 	if (optionNumber === 1) {
-		pcChoice = "Rock";
+		pcChoice = 'Rock';
 	} else if (optionNumber === 2) {
-		pcChoice = "Paper";
+		pcChoice = 'Paper';
 	} else {
-		pcChoice = "Scissors";
+		pcChoice = 'Scissors';
 	}
 
 	pcDisplay.textContent = pcChoice;
@@ -46,15 +46,15 @@ const computerDecision = () => {
 
 // APPLY EVENT LISTENER TO OPTIONS AND START GAME
 options.forEach((option) => {
-	option.addEventListener("click", (e) => {
+	option.addEventListener('click', (e) => {
 		// COLLECT AND DISPLAY PLAYER CHOICE
 		playerChoice = e.target.id;
-		if (playerChoice === "rock") {
-			playerDisplay.textContent = "Rock";
-		} else if (playerChoice === "paper") {
-			playerDisplay.textContent = "Paper";
-		} else if (playerChoice === "scissors") {
-			playerDisplay.textContent = "Scissors";
+		if (playerChoice === 'rock') {
+			playerDisplay.textContent = 'Rock';
+		} else if (playerChoice === 'paper') {
+			playerDisplay.textContent = 'Paper';
+		} else if (playerChoice === 'scissors') {
+			playerDisplay.textContent = 'Scissors';
 		}
 
 		// CALL PC DECISION
